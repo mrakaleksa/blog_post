ecc4fe578505180f355ff5859096b2bf3b216578
Aleksa Mrakovic <mrakovic.aleksa@gmail.com>
2024-07-11 23:13:31 +0200

    categoty model

--- blog_post/database/migrations/2024_07_11_160251_create_categories_table.php
+++ blog_post/database/migrations/2024_07_11_160251_create_categories_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class ChangeUsernamesToUsernameInUsersTable extends Migration
+class CreateCategoriesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,9 +13,11 @@ class ChangeUsernamesToUsernameInUsersTable extends Migration
      */
     public function up()
     {
-        Schema::table('users', function (Blueprint $table) {
-            //
-            $table->renameColumn('usernames', 'username');
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug');
+            $table->timestamps();
         });
     }
 
@@ -26,9 +28,6 @@ class ChangeUsernamesToUsernameInUsersTable extends Migration
      */
     public function down()
     {
-        Schema::table('users', function (Blueprint $table) {
-            //
-            $table->renameColumn('usernames', 'username');
-        });
+        Schema::dropIfExists('categories');
     }
 }
