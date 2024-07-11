12965aa7dbc5ccc2b2570a7a2468fa6686dc4a82
Aleksa Mrakovic <mrakovic.aleksa@gmail.com>
2024-07-11 23:57:01 +0200

    dodavanje relacija

--- blog_post/app/Models/Post.php
+++ blog_post/app/Models/Post.php
@@ -14,4 +14,14 @@ class Post extends Model
         'excerpt',
         'body',
     ];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
