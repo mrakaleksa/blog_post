62b1461a891121c1297ddcaed45ded0dd7516993
Aleksa Mrakovic <mrakovic.aleksa@gmail.com>
2024-07-12 03:15:04 +0200

    stuff

--- blog_post/database/factories/PostFactory.php
+++ blog_post/database/factories/PostFactory.php
@@ -3,6 +3,8 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use \App\Models\User;
+use \App\Models\Category;
 
 class PostFactory extends Factory
 {
@@ -14,7 +16,12 @@ class PostFactory extends Factory
     public function definition()
     {
         return [
-            //
+            'title' => $this->faker->title(),
+            'excerpt' => $this->faker->sentence(),
+            'body' => $this->faker->paragraph(),
+            'slug' => $this->faker->slug(),
+            'user_id' => User::factory(),
+            'category_id' => Category::factory(),
         ];
     }
 }
--- blog_post/database/seeders/DatabaseSeeder.php
+++ blog_post/database/seeders/DatabaseSeeder.php
@@ -18,47 +18,8 @@ class DatabaseSeeder extends Seeder
     {
         Category::truncate();
         User::truncate();
+        Post::truncate();
 
-        $user = User::factory()->create();
-
-        $cat1 = Category::create([
-            'name' => 'Personal',
-            'slug' => 'personal'
-        ]);
-        $cat2 = Category::create([
-            'name' => 'Work',
-            'slug' => 'work'
-        ]);
-        $cat3 = Category::create([
-            'name' => 'Fun',
-            'slug' => 'fun'
-        ]);
-
-        $post1 = Post::create([
-            'title' => 'Work Post',
-            'excerpt' => 'Work Post excerpt',
-            'body' => 'Work Post content',
-            'slug' => 'work-post',
-            'user_id' => $user->id,
-            'category_id' => $cat2->id
-        ]);
-
-        $post2 = Post::create([
-            'title' => 'Personal Post',
-            'excerpt' => 'Personal Post excerpt',
-            'body' => 'Personal Post content',
-            'slug' => 'personal-post',
-            'user_id' => $user->id,
-            'category_id' => $cat1->id
-        ]);
-
-        $post3 = Post::create([
-            'title' => 'Fun Post',
-            'excerpt' => 'Fun Post excerpt',
-            'body' => 'Fun Post content',
-            'slug' => 'fun-post',
-            'user_id' => $user->id,
-            'category_id' => $cat3->id
-        ]);
+        Post::factory(5)->create();
     }
 }
