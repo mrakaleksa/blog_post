3a0f1927ee234467551633bbeba5da579ec57dc3
Aleksa Mrakovic <mrakovic.aleksa@gmail.com>
2024-07-12 02:58:18 +0200

    seeding database

--- blog_post/database/seeders/DatabaseSeeder.php
+++ blog_post/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use \App\Models\User;
+use \App\Models\Category;
+use \App\Models\Post;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,49 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        Category::truncate();
+        User::truncate();
+
+        $user = User::factory()->create();
+
+        $cat1 = Category::create([
+            'name' => 'Personal',
+            'slug' => 'personal'
+        ]);
+        $cat2 = Category::create([
+            'name' => 'Work',
+            'slug' => 'work'
+        ]);
+        $cat3 = Category::create([
+            'name' => 'Fun',
+            'slug' => 'fun'
+        ]);
+
+        $post1 = Post::create([
+            'title' => 'Work Post',
+            'excerpt' => 'Work Post excerpt',
+            'body' => 'Work Post content',
+            'slug' => 'work-post',
+            'user_id' => $user->id,
+            'category_id' => $cat2->id
+        ]);
+
+        $post2 = Post::create([
+            'title' => 'Personal Post',
+            'excerpt' => 'Personal Post excerpt',
+            'body' => 'Personal Post content',
+            'slug' => 'personal-post',
+            'user_id' => $user->id,
+            'category_id' => $cat1->id
+        ]);
+
+        $post3 = Post::create([
+            'title' => 'Fun Post',
+            'excerpt' => 'Fun Post excerpt',
+            'body' => 'Fun Post content',
+            'slug' => 'fun-post',
+            'user_id' => $user->id,
+            'category_id' => $cat3->id
+        ]);
     }
 }
